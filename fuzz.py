b6aa3d722a325263dc192276a724a4bc542652fd
advanced12iq <kkrasavecc@gmail.com>
2024-12-14 16:38:25 +0300

    added: hnf begin

--- fuzz.py
+++ fuzz.py
@@ -1,4 +1,7 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+import string
+import sys
+sys.setrecursionlimit(10000)
 
 def get_brackets(l):
     newList = []
@@ -16,6 +19,7 @@ def get_brackets(l):
     
     return newList
 
+
 def read(lines):
     rules = []
     nonTerms = defaultdict(list)
@@ -23,7 +27,52 @@ def read(lines):
         rules.append(list(map(lambda r: r.strip().split(), rule.strip().split('- >'))))
     for rule in rules:
         nonTerms[rule[0][0]] += [get_brackets(rule[1])]
-    print(rules)
     
-    print(nonTerms)
 
+    def delete_long_rules(root):
+        counter = 1
+        for i in range(len(nonTerms[root])):
+            rule = nonTerms[root][i]
+            if len(rule) > 2:
+                new_node = root + str(counter)
+                nonTerms[new_node] = [rule[1:]]
+                new_rules = [rule[0], new_node]
+                nonTerms[root][i] = [new_rules]
+                delete_long_rules(new_node)
+                counter += 1
+                
+    for nonTerm in list(nonTerms.keys()):
+        delete_long_rules(nonTerm)
+
+    isEpsilon = defaultdict(bool)
+    concernedRules = defaultdict(list)
+    counter = defaultdict(int)
+    Q = deque()
+
+    grammar = [(s, l) for s in nonTerms.keys() for l in nonTerms[s]]
+
+    print(grammar)
+    
+
+    print(nonTerms)
+    
+    
+    # def find_generative_element(node):
+    #     visited.add(node)
+    #     for verticies in nonTerms[node]:
+    #         for vertex in verticies:
+    #             if vertex[0].islower():
+    #                 continue
+    #             if vertex not in visited:
+    #                 find_generative_element(vertex)
+
+
+    # max_element = (0, 0)
+
+    # for key in nonTerms.keys():
+    #     visited = set([])
+    #     find_generative_element(key)
+    #     if len(visited) > max_element[0]:
+    #         max_element=(len(visited), key)        
+    
+    # generative_element = max_element[1]
